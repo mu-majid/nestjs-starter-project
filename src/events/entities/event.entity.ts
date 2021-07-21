10304ce893dcfbc5366ad0c62b12b48948839dfd
Muhammad Majid <mmajidkamel@gmail.com>
2021-07-21 19:30:18 +0200

    added index to an entity

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,10 +1,13 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, Index, PrimaryGeneratedColumn } from "typeorm";
 
+// Composite Index
+@Index(['name', 'type'])
 @Entity()
 export class Event {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Index()
   @Column()
   name: string;
 
